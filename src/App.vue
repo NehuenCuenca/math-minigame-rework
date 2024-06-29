8ce62ee946344914d4324c89b3eeec81dcc94dcd
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-06-29 13:30:27 -0300

    Add the menu or initial screen

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header class="header">
+    <h1 class="header__title">Mini-juego de Matemáticas</h1>
+    <button type="button" class="header__theme-toggler">🌑/☀</button>
+  </header>
+  <main class="main-menu">
+    <div class="menu-buttons">
+      <button type="button" class="primary-button">Empezar juego</button>
+      <button type="button" class="terciary-button">Leer tutorial</button>
+    </div>
+  </main>
+  <footer class="footer">
+    Desarrollado por <a href="https://www.linkedin.com/in/nehuen-cuenca/" target="_blank" rel="noopener noreferrer">Nehuen Cuenca</a>
+  </footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+.header{
+  max-height: 20vh;
+  padding: 1rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 3px solid var(--sky-2);
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.header__title{
+  font: normal normal 400 clamp(2rem, 5vw, 2rem) var(--display-font);
+  overflow-wrap: break-word;
+  color: var(--color-font );
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.header__theme-toggler{
+  color: var(--color-font );
+}
+
+.main-menu{
+  padding: 2rem 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.menu-buttons{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+}
+
+.primary-button,
+.terciary-button{
+  font: normal normal 400 clamp(1.8rem, 5vw, 2rem) var(--display-font);
+  border: none;
+  border-radius: 5px;
+}
+
+.primary-button{
+  background-color: var(--color-font);
+  color: var(--color-bg);
+  padding: .5rem 1rem;
+  box-shadow: 2px 2px 12px 1px rgba(0, 0, 0, 0.7);
+}
+.terciary-button{
+  background-color: transparent;
+  color: var(--color-font);
+}
+
+.footer{
+  max-height: 10vh;
+  font: normal normal 400 clamp(1.3rem, 5vw, 1.6rem) var(--display-font);
+  text-align: center;
+  padding: 1rem;
 }
 </style>
