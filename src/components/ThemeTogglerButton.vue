60bd90fe339450557ebfef8fe169f14dab844540
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-07-18 18:27:16 -0300

    Add a theme toggler button

---
+++ src/components/ThemeTogglerButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button type="button" class="header__theme-toggler" @click="changeTheme">
+      <i v-if="currentTheme === 'light'" class='bx bxs-moon bx-md'></i> 
+      <i v-else class='bx bxs-sun bx-md'></i>
+    </button>
+</template>
+
+<script setup lang="ts">
+    import { ref, computed } from 'vue';
+
+    const htmlElement  = ref(document.querySelector("html"))
+    const currentTheme = ref(document.querySelector("html").getAttribute("data-theme"))
+    
+    const changeTheme = () => {
+        if(currentTheme.value === 'light'){
+            htmlElement.value.setAttribute("data-theme", 'dark')
+            currentTheme.value = 'dark'
+        } else {
+            htmlElement.value.setAttribute("data-theme", 'light')
+            currentTheme.value = 'light'
+        }  
+    }
+
+</script>
+
+<style scoped>
+
+</style>computed, computed, 
