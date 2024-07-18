40e6e93a0dbb72b3225dfbefffe4ca127d1254bb
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-07-18 17:12:54 -0300

    Add the calculations carousel with the corrected calc, the current calc and the next calc

---
+++ src/components/CalculationItem.vue
@@ -0,0 +1,71 @@
+<template>
+    <li class="calculation-item">
+        <span 
+            class="calculation-item__text" 
+            :class="isBeingResolved ? 'calculation-item__text_being-resolved' : ''"
+        >
+            {{ calculation.operation }}
+        </span>
+        <template v-if="!isBeingResolved && calculation.result != null">
+            <i v-if="calculation.isResultCorrect" class='bx bx-check calculation-item__icon'></i>
+            <i v-else class='bx bx-x calculation-item__icon'></i>
+        </template>
+    </li>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps({
+        calculation: {
+            type: Object,
+            required: true,
+            default: {
+                operation: '',
+                result: null,
+                isResultCorrect: false
+            }
+        },
+        isBeingResolved: {
+            type: Boolean,
+            // required: true,
+            default: false
+        },
+        isResultCorrect: {
+            type: Boolean,
+            default: false
+        },
+    })
+</script>
+
+<style scoped>
+.calculation-item{
+    min-height: 50px;
+    min-width: 80px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.calculation-item__text{
+    font: normal normal 400 clamp(1.4rem, 5vw, 1.5rem) var(--display-font);
+    opacity: 0.6;
+    color: var(--color-font);
+}
+
+.calculation-item__text_being-resolved{
+    font: normal normal 400 2.5rem var(--display-font);
+    opacity: 1;
+}
+
+.calculation-item__icon{
+    font-size: 4rem;
+    position: absolute;
+}
+
+.bx-check{
+    color: green;
+}
+.bx-x{
+    color: red;
+}
+</style>
---
+++ src/components/CalculationsCarousel.vue
@@ -0,0 +1,73 @@
+<template>
+    <ul class="calculations-carousel">
+        <CalculationItem 
+            :calculation="calculations[currentCalculationIndex-1]" />
+        <CalculationItem 
+            :calculation="calculations[currentCalculationIndex]" 
+            :isBeingResolved="true"
+            class="current-calculation">
+            <p>¿Cuanto es?</p>
+        </CalculationItem>
+        <CalculationItem :calculation="calculations[currentCalculationIndex+1]" />
+    </ul>
+</template>
+
+<script setup lang="ts">
+    import { computed } from "vue";
+    import CalculationItem from "../components/CalculationItem.vue";
+
+    const props = defineProps({
+        calculations: {
+            type: Array,
+            required: true,
+        },
+        currentCalculationIndex: {
+            type: Number,
+            required: true,
+        }
+    })
+</script>
+
+<style scoped>
+.calculations-carousel {
+    background: transparent;
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+    /* display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center; */
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 2rem 0;
+}
+
+.calculations-carousel::before, .calculations-carousel::after {
+    background: linear-gradient(to right, var(--color-details) 0%, rgba(255, 255, 255, 0) 100%);
+    content: "";
+    height: 100%;
+    position: absolute;
+    width: 100px;
+    width: clamp(100px, 40vw, 350px);
+    z-index: 2;
+}
+
+.calculations-carousel::after {
+    right: 0;
+    top: 0;
+    -webkit-transform: rotateZ(180deg);
+    transform: rotateZ(180deg);
+}
+
+.calculations-carousel::before {
+    left: 0;
+    top: 0;
+}
+
+
+/* .current-calculation{
+    background-color: brown;
+} */
+
+</style>
